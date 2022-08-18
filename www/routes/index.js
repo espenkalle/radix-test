8efa12e846687a7e896457d0d211c65294bdcc80
espenkalle <eskal@equinor.com>
2022-08-18 13:36:05 +0200

    test

--- www/routes/index.js
+++ www/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    console.log('hei');
+    console.log('Halla');
     res.render('index', { title: 'Radix Workshop' });
 });
 
